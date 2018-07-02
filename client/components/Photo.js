ddbb19a0f25dc1d44ab6790cab76d3701e2f244d
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-07-02 13:59:52 -0700

    Added redux store and hooked up to app

---
+++ client/components/Photo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Photo extends Component {
+  render() {
+    return (
+      <div className='grid-figure'>
+        I am photo
+  
+      </div>
+    )
+  }
+}
+
+export default Photo;
--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import Photo from './Photo';
 
 class PhotoGrid extends Component {
   render() {
     return (
-      <div>
-        <h2>photo</h2>
+      <div className='photo-grid'>
+        {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
       </div>
     )
   }
